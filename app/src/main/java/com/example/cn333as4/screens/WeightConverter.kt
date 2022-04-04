c1607366f145872611d28e019cdd018abed8a74d
poottipong rueangsuntikornkul <6210520117@student.tu.ac.th>
2022-04-04 21:36:26 +0700

    weight converter left

---
+++ app/src/main/java/com/example/cn333as4/screens/WeightConverter.kt
@@ -0,0 +1,23 @@
+package com.example.cn333as4.screens
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.cn333as4.R
+import com.example.cn333as4.viewmodels.WeightViewModel
+
+
+class WeightConverter {}
